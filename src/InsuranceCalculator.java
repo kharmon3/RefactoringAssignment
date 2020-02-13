bee0267e1e1df63748fa175ff8c2d51515633d2d
Kylie Harmon <kharmon3@bsu.edu>
2020-02-13 11:59:30 -0500

    Fine tuning

--- src/InsuranceCalculator.java
+++ src/InsuranceCalculator.java
@@ -1,6 +1,6 @@
 public class InsuranceCalculator {
 
-    private final InsuranceStrategyVeryHigh insuranceStrategyVeryHigh = new InsuranceStrategyVeryHigh();
+    private InsuranceStrategyVeryHigh strategy;
 
     public double calculateInsurance(double income)
     {
@@ -11,7 +11,9 @@ public class InsuranceCalculator {
         } else if (income <= 60000) {
             return (income-30000)*0.1+76500;
         } else {
-            return insuranceStrategyVeryHigh.calculateInsuranceVeryHigh(income);
+
+            strategy = new InsuranceStrategyVeryHigh();
+            return strategy.calculateInsuranceVeryHigh(income);
         }
     }
 }
